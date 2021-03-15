9c97fd49d6f41deabdcdd862be8ca7281887ebec
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2021-03-15 12:33:08 -0400

    Basic Class for now for the objects
    Its good for now unless if we want to
    -add health to objects
    -add a lifespan to them
    -add anything to them

--- root/elements/Map/map_Class.js
+++ root/elements/Map/map_Class.js
@@ -1,1 +1,23 @@
-//Classes for the map mainly for layout / objects 
+//Classes for the map mainly for objects
+
+var objects = [];
+//@class Object : class to form objects
+//@Constructor
+////@param location [object] {Restricted : {x:#,y:#}} : location of the top left corner of the object
+class Object{
+    constructor(location, width,height){
+        this.location = location;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+
+//@function formObject : makes a new object
+//@param location [object] {restricted : {x:#,y:#}} : location of the object
+//@param width/height [integer] : Width/height of the object 
+function formObject(location, width, height) {
+    var newObject = new Object(location,width,height);//makes a new one
+    objects.push(newObject);//object push to be able to catelog them 
+    return newObject;//return so that it logs 
+}
