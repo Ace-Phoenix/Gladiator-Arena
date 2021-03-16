f305684b3bd2402378443bf638fb1e6476772f6c
Euan Barclay <Silverswimmer63@gmail.com>
2021-03-16 12:02:35 -0400

    added the classes

--- root/elements/NPC/ai_NPC.js
+++ root/elements/NPC/ai_NPC.js
@@ -1,6 +1,14 @@
 //class for the creation of the NPC placing and what not
-//-start location
-//-health
-//-damage
-//-spawn rate?
-//Big class that uses the ai_Class.js to make an AI 
+//-start location(pos)
+//-health(hp)
+//-damage(dam)
+//-spawn rate?(spRate)
+//Big class that uses the ai_Class.js to make an AI
+class Npc {
+  constructor(pos, hp, dam, spRate) {
+    this.pos = pos;
+    this.hp = hp;
+    this.dam = dam;
+    this.spRate = spRate;
+  }
+}
--- root/elements/Player/player_Class.js
+++ root/elements/Player/player_Class.js
@@ -1,5 +1,14 @@
 //Player class for making the player
-//-start location
-//-Health
-//-Damage
-//-Lives?
+//-start location (pos)
+//-Health (hp)
+//-Damage (dam)
+//-Lives? (lives)
+
+class Player {
+  constructor(pos, hp, dam, lives) {
+    this.pos = pos;
+    this.hp = hp;
+    this.dam = dam;
+    this.lives = lives;
+  }
+}
