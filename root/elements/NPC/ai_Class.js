718a81097a5c077c37b251b796e57b3fbc221a15
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2021-03-16 12:21:07 -0400

    fixed some things

--- root/elements/NPC/ai_Class.js
+++ root/elements/NPC/ai_Class.js
@@ -6,3 +6,28 @@
 //-Peasants - Small Brain - Slow - Small low ranged attacks
 //-Gladiators - Medium Brain - medium Speed - Medium Range
 //all needing a different class for each
+
+class MakeAI{
+    constructor(type,rootName){
+        this.type = type;
+        this.root = rootName;
+        this.difficulty = this.getInstructions(type);
+        this.instruct = this.getInstructions(type);
+    }
+    getInstructions(type){
+        if (type == "Gladiator") {
+            this.difficulty = "adept";
+        }
+        if (type == "Tiger") {
+            this.difficulty = "advanced";
+        }
+        if (type == "Peasant") {
+            this.difficulty = "Low";
+        }
+        if (type == "Boss") {
+            this.difficulty = "Extreme";
+        }
+    }
+    
+    
+}
