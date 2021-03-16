9da692d23168e8dfe074049c9a7c9675e1c2468e
Euan Barclay <Silverswimmer63@gmail.com>
2021-03-16 11:55:13 -0400

    start&stop

--- root/functionality/start&stop/start&stop.js
+++ root/functionality/start&stop/start&stop.js
@@ -1,2 +1,6 @@
 //Functions that tell the game to update and also to start or end game theoretically this will be
 //done last and used as a test until everything else is done
+var gameState = false;
+while (gameState == true) {
+  //game run
+}
