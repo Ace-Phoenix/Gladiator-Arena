134005f6cca1be74f0eb51c59d19ec81e98eaeb2
Owen <74560237+Nickel6969@users.noreply.github.com>
2021-03-17 11:52:43 -0400

    Update player_Placements.js

--- root/functionality/start&stop/placements/player_Placements.js
+++ root/functionality/start&stop/placements/player_Placements.js
@@ -1,1 +1,9 @@
-//this will not only place but also update the player [movement]
+//this will not only place but also update the player [movement]function drawCircle() { //This function will draw the player that will be controlled by the player
+
+  ctx.beginPath();//begins to draw ball on the canvas
+  ctx.arc(player.xPos, player.yPos, player.rad, 0, Math.PI*2); //Draws the player using it's pre-determined loation and radius.
+  ctx.fillStyle = "clear";
+
+  ctx.drawImage(player, player.xPos - 19, ball.yPos - 19, 45, 35);
+  ctx.stroke();
+}
