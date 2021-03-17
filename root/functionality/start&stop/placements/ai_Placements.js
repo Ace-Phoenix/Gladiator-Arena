fc37117926c02f07b07501362d0ac1602710079d
arev11 <74614760+arev11@users.noreply.github.com>
2021-03-17 12:22:36 -0400

    Update ai_Placements.js

--- root/functionality/start&stop/placements/ai_Placements.js
+++ root/functionality/start&stop/placements/ai_Placements.js
@@ -1,1 +1,8 @@
 //ai placements will take the AI class and npc class and use them to place them
+function placeNpc(pos, hp, dam, spRate){
+
+   ctx.beginPath();//begins to draw ball on the canvas
+   ctx.arc(npc.xPos, npc.yPos, npc.rad, 0, Math.PI*2); //Draws the player using it's pre-determined loation and radius.
+   ctx.fillStyle = "clear";
+   ctx.drawImage(npc, npc.xPos - 19, npc.yPos - 19, 45, 35);
+   ctx.stroke();
