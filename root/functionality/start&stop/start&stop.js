b83c3ce6e3f5dd835411bdbb3aeb5c829df8b9e1
Euan Barclay <Silverswimmer63@gmail.com>
2021-03-17 12:14:47 -0400

    added potentiol for continuouse movement

--- root/functionality/start&stop/start&stop.js
+++ root/functionality/start&stop/start&stop.js
@@ -1,6 +1,44 @@
 //Functions that tell the game to update and also to start or end game theoretically this will be
 //done last and used as a test until everything else is done
 var gameState = false;
+var up = false;//variable to detect if the w key is peing pressed down
+var down = false;//variable to detect if the s key is peing pressed down
+var left = false;//variable to detect if the a key is peing pressed down
+var right = false;//variable to detect if the d key is peing pressed down
+
 while (gameState == true) {
   //game run
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "w") {
+    up = true;
+  }
+  if (e.key == "s") {
+    down = true;
+  }
+  if (e.key == "a") {
+    left = true;
+  }
+  if (e.key == "d") {
+    right = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "w") {
+    up = false;
+  }
+  if (e.key == "s") {
+    down = false;
+  }
+  if (e.key == "a") {
+    left = false;
+  }
+  if (e.key == "d") {
+    right = false;
+  }
 }
