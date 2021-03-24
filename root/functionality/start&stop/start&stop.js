10a0510f18303be156440b0672e08a33732bb49e
Euan Barclay <Silverswimmer63@gmail.com>
2021-03-24 11:33:43 -0400

    comments in start&stop

--- root/functionality/start&stop/start&stop.js
+++ root/functionality/start&stop/start&stop.js
@@ -19,9 +19,11 @@ while (gameState == true) {
   //game run
 }
 
+//listen to see if a key has been pressed. If yes, run fuction
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 
+//If the key is pressed down, set the movment direction corredponding to the key to true
 function keyDownHandler(e) {
   if (e.key == "w") {
     up = true;
@@ -37,6 +39,7 @@ function keyDownHandler(e) {
   }
 }
 
+//If the key is released,  set the movment direction corredponding to the key to false
 function keyUpHandler(e) {
   if (e.key == "w") {
     up = false;
@@ -69,45 +72,43 @@ function fileHandeler() {
   //Tells stage handler what stage to use
   ctx.clearRect(0, 0, c.width, c.height);
   setLocations(player);
-  if(right == true) {
+  if(right == true) {//Allows the player to move to the right
     player.xPos += 1;
-    //this is to prevent the player from running off the map. Its in a comment because I don't know the width of the player
-    if (player.xPos + 10 > c.width){
+    if (player.xPos + 10 > c.width){// this is to prevent the player from running off the map
       player.xPos = c.width - 10;
     }
   }
-  else if(left == true) {
+  else if(left == true) {//Allows the player to move to the left
     player.xPos -= 1;
-    if (player.xPos-10 < 0){
+    if (player.xPos-10 < 0){// this is to prevent the player from running off the map
       player.xPos = 10;
     }
   }
-  if (down == true) {
+  if (down == true) {//Allows the player to move down
     player.yPos += 1;
-    // this is to prevent the player from running off the map. Its in a comment because I don't know the width of the player
-    if (player.yPos + 10 > c.width){
+    if (player.yPos + 10 > c.width){// this is to prevent the player from running off the map
       player.yPos = c.width - 10;
     }
   }
-  else if (up == true) {
+  else if (up == true) {//Allows the player to move up
     player.yPos -= 1;
-    if (player.yPos-10 < 0) {
+    if (player.yPos-10 < 0) {// this is to prevent the player from running off the map
       player.yPos = 10;
     }
   }
-  for (var i = 0; i < stage.objects.amount; i++) {
+  for (var i = 0; i < stage.objects.amount; i++) {//Checks to see if the player is atempting to move into a spot that is should not be
     if ((player.xPos >= stage.objects.locations[i].x && player.xPos <= stage.objects.locations[i].x + 30) || (player.xPos + 10 >= stage.objects.locations[i].x && player.xPos + 10 <= stage.objects.locations[i].x + 30) || (player.xPos - 10 >= stage.objects.locations[i].x && player.xPos - 10 <= stage.objects.locations[i].x + 30)) {
       if ((player.yPos >= stage.objects.locations[i].y && player.yPos <= stage.objects.locations[i].y + 30) || (player.yPos - 10 >= stage.objects.locations[i].y && player.yPos - 10 <= stage.objects.locations[i].y + 30) || (player.yPos + 10 >= stage.objects.locations[i].y && player.yPos + 10 <= stage.objects.locations[i].y + 30)) {
-        if(right == true) {
+        if(right == true) {//prevents the player from going inside of objects that are suposed to be soild
           player.xPos -= 1;
         }
-        else if(left == true) {
+        else if(left == true) {//prevents the player from going inside of objects that are suposed to be soild
           player.xPos += 1;
         }
-        if (down == true) {
+        if (down == true) {//prevents the player from going inside of objects that are suposed to be soild
           player.yPos -= 1;
         }
-        else if (up == true) {
+        else if (up == true) {//prevents the player from going inside of objects that are suposed to be soild
           player.yPos += 1;
         }
       }
