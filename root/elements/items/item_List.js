e3a61107a54422c7a3de9c4a2cd9d59c9d5155f1
Euan Barclay <Silverswimmer63@gmail.com>
2021-03-29 12:28:12 -0400

    added model items

---
+++ root/elements/items/item_List.js
@@ -0,0 +1,26 @@
+const Weapon = [
+  {
+    name: "Simple Sword", //name
+    type: Sword, //type of weapon
+    value: 1, //Items value
+    decs: "A simple but effective sword", // A short description of the item
+    dam: 2, //How much damage it does
+  },
+  {
+    name: "Simple Shield",
+    type: Shield,
+    value: 1,
+    desc: "A basic shield for some protection",
+    dam: 1,
+    damRest: 1, //How much damage it resists/ block power/ armor
+  }
+]
+ const Armor = [
+   {
+     name: "Basic Chestplate",
+     type: Chestplate,
+     value: 1,
+     desc: "Basic protection from attacks",
+     damRest: 2,
+   }
+ ]
